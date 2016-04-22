f278912a048c26da617c7c987b7a4c1a6b2aac56
gbarnesus <gbarnesus@gmail.com>
2016-04-21 18:48:50 -0700

    admin dashboard created
    cool

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var express = require('express'),
       lastName: String,
       email: {type: String, unique: true},
       password: String,
+      admin: String,
     }));
 //connect to mongo
 mongoose.connect('mongodb://localhost/wcntOnline');
@@ -34,13 +35,23 @@ mongoose.connect('mongodb://localhost/wcntOnline');
   duration: 30 * 60 * 1000,
   activeDuration: 5 * 60 * 1000,
 }));
+  app.use(sessions({
+    cookieName: 'admin',
+    secret: 'sdfgsdjgsdfsdr4356gdfgdfg',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+  }));
 
  app.use(csrf());
+
   app.use(function(req, res, next){
+    console.log(req.session.user)
+    console.log(req.admin.user)
   if (req.session && req.session.user) {
     User.findOne({email: req.session.user.email}, function(err, user){
       if(user) {
         req.user = user;
+
         delete req.user.password;
         req.session.user = req.user;
         res.locals.user = req.user;
@@ -53,11 +64,20 @@ mongoose.connect('mongodb://localhost/wcntOnline');
 });
 function requireLogin(req, res, next){
   if (!req.user){
+
+    res.redirect('/login' )
+  } else {
+    next();
+  };
+};
+function requireAdminLogin(req, res, next){
+  if (!req.admin.user){
     res.redirect('/login')
   } else {
     next();
   };
 };
+
 
 
 //get routes
@@ -72,6 +92,7 @@ function requireLogin(req, res, next){
       });
       app.get('/logout', function(req, res){
         req.session.reset()
+        req.admin.reset()
         res.redirect('/');
       });
     app.get('/projectDash', requireLogin, function(req, res){
@@ -92,22 +113,58 @@ function requireLogin(req, res, next){
     app.get('/projectdocs', requireLogin, function(req, res){
       res.render('projectdocs');
     })
+    app.get('/adminDashboard', requireAdminLogin, function(req, res){
+      res.render('adminDashboard', {csrfToken: req.csrfToken()});
+    })
+    app.get('/register', function(req, res){
+      res.render('register', {csrfToken: req.csrfToken()});
+    })
+
+
 
 
 //post routes
 app.post('/login',  function(req, res){
   User.findOne({email: req.body.email}, function(err, user){
       if(!user) {
-        res.render('dashboard', {error: 'Invalid email or Password'});
+        res.render('login', {error: 'Invalid email or Password'});
       } else {
         if (bcrypt.compareSync(req.body.password, user.password)){
-          req.session.user = user;
-          res.redirect('/dashboard')
+          if(user.admin === "yes"){
+            req.session.user = user;
+            req.admin.user = user;
+            res.redirect('/adminDashboard')
+          } else {
+            req.session.user = user;
+            res.redirect('/dashboard')
+          }
+
+
         } else {
           res.render('login.jade', {error: 'Invalid email or Password', csrfToken: req.csrfToken()});
         }
       }
     })
+});
+app.post('/register', function(req, res){
+  var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+  var user = new User({
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    email : req.body.email,
+    admin: req.body.admin,
+    password : hash
+  });
+  user.save(function(err){
+  if(err){
+    var err = "something bad happend! Try Again!"
+    if (err === 11000) {
+      var error = "that email is allready in use"
+    }
+    res.render('register.jade', {error: error});
+  }
+  res.redirect('/dashboard');
+})
 });
 
 
